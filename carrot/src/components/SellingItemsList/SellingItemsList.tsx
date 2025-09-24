b4eabb6191f122f12b2905f07cb3227a47130a95
허윤아 <aquahua@gmail.com>
2025-09-24 16:11:53 +0900

    [0924] data.json 파일 재수정

--- carrot/src/components/SellingItemsList/SellingItemsList.tsx
+++ carrot/src/components/SellingItemsList/SellingItemsList.tsx
@@ -1,13 +1,31 @@
+// src/components/SellingItemsList/SellingItemsList.tsx
 import SellingItems from "../../components/SellingItems/SellingItems";
 import * as S from "./SellingItemsListStyle";
+import data from "../../data/data.json";
 
-function SellingItemsList() {
+interface SellingItemsListProps {
+  sellerName: string;
+  currentProductId: number;
+}
+
+export default function SellingItemsList({
+  sellerName,
+  currentProductId,
+}: SellingItemsListProps) {
+  // 판매자명이 같은 상품만, 현재 상품 제외
+  const filteredProducts = data.products.filter(
+    (p) => p.seller.name === sellerName && p.id !== currentProductId
+  );
+
   return (
     <S.ListHugger>
-      <SellingItems />
-      {/* 아직 작성중입니다! 세부페이지 아래쪽 리스트 컴포넌트입니다 */}
+      {filteredProducts.length > 0 ? (
+        filteredProducts.map((product) => (
+          <SellingItems key={product.id} product={product} />
+        ))
+      ) : (
+        <div>판매자가 등록한 다른 상품이 없습니다.</div>
+      )}
     </S.ListHugger>
   );
 }
-
-export default SellingItemsList;
